2f4b383230bdbf22f43bcbe7fd7db5b3e3146714
Moo-MSN <160102275+Moo-MSN@users.noreply.github.com>
2025-09-02 08:33:21 +0700

    Edit and add "home and contact page"

--- src/views/users/ContactView.vue
+++ src/views/users/ContactView.vue
@@ -8,17 +8,50 @@ import UserLayout from "@/components/layouts/UserLayout.vue";
       <h1 class="font-bold text-5xl">Clinic name</h1>
       <h2 class="font-semibold text-2xl mt-4">คลินิกทำฟัน ทันตกรรมเด็ก จัดฟันขอนแก่น โดยทันตแพทย์เฉพาะทาง</h2>
     </div>
-    
+
     <div>
-<div class="grid grid-cols-2 gap-4 my-20">
+      <div class="grid grid-cols-2 gap-10 my-20 p-4">
         <div>
-            <h1>ที่อยู่ติดต่อ</h1>
+          <h1 class="text-2xl font-semibold">ที่อยู่ติดต่อ</h1>
+          <div class="text-xl p-4">
+            <p>
+              <span> 123 หมู่ 4 ตำบลบ้านเป็ด อำเภอเมืองขอนแก่น จังหวัดขอนแก่น 40000 </span>
+            </p>
+          </div>
+
+          <div class="text-xl p-4">
+            <p>
+              <span> โทรศัพท์: 043-123456, 081-2345678 </span>
+            </p>
+          </div>
+
+          <div class="text-xl p-4">
+            <p>
+              <span> เวลาเปิด - ปิด </span>
+            </p>
+          </div>
+
+          <div class="text-xl p-4">
+            <p>
+              <span> อยู่ใกล้เคียง </span>
+            </p>
+          </div>
         </div>
-        <div>
-            Google Map: https://goo.gl/maps/xyz
-        </div>
+
+        <div>Google Map: https://goo.gl/maps/xyz</div>
+      </div>
+
+      <div class=" items-center justify-center flex gap-10 p-20">
+      <!-- Facebook -->
+      <button class="btn bg-[#1A77F2] text-white border-[#005fd8] w-60 h-20 text-2xl ">
+        <svg aria-label="Facebook logo" width="40" height="40" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32">
+          <path fill="white" d="M8 12h5V8c0-6 4-7 11-6v5c-4 0-5 0-5 3v2h5l-1 6h-4v12h-6V18H8z"></path>
+        </svg>
+        
+        Facebook
+      </button>
+
+      </div>
     </div>
-    </div>
-    
   </UserLayout>
 </template>
--- src/views/users/HomeView.vue
+++ src/views/users/HomeView.vue
@@ -1,12 +1,35 @@
 <script setup>
-import UserLayout from '@/components/layouts/UserLayout.vue';
-
+import UserLayout from "@/components/layouts/UserLayout.vue";
 </script>
 
 <template>
-    <UserLayout>
-        <div>
-            HomeView
+  <UserLayout>
+    <div class="divide-y divide-gray-200 my-0.5">
+      <div class="carousel w-full">
+        <div id="item1" class="carousel-item w-full">
+          <img src="https://img.daisyui.com/images/stock/photo-1625726411847-8cbb60cc71e6.webp" class="w-full" />
         </div>
-    </UserLayout>
+        <div id="item2" class="carousel-item w-full">
+          <img src="https://img.daisyui.com/images/stock/photo-1609621838510-5ad474b7d25d.webp" class="w-full" />
+        </div>
+        <div id="item3" class="carousel-item w-full">
+          <img src="https://img.daisyui.com/images/stock/photo-1414694762283-acccc27bca85.webp" class="w-full" />
+        </div>
+        <div id="item4" class="carousel-item w-full">
+          <img src="https://img.daisyui.com/images/stock/photo-1665553365602-b2fb8e5d1707.webp" class="w-full" />
+        </div>
+      </div>
+      <div class="flex w-full justify-center gap-2 py-2">
+        <a href="#item1" class="btn btn-circle">1</a>
+        <a href="#item2" class="btn btn-circle">2</a>
+        <a href="#item3" class="btn btn-circle">3</a>
+        <a href="#item4" class="btn btn-circle">4</a>
+      </div>
+
+      <div class="flex flex-col items-center justify-center h-full my-20">
+      <h1 class="font-bold text-5xl">Clinic name</h1>
+      <h2 class="font-semibold text-2xl mt-4">คลินิกทำฟัน ทันตกรรมเด็ก โดยทันตแพทย์เฉพาะทาง</h2>
+    </div>
+    </div>
+  </UserLayout>
 </template>
